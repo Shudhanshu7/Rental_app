4b09680e3c8d9f9899f8367b78c793a295a3ce2b
Shudhanshu7 <149239101+Shudhanshu7@users.noreply.github.com>
2024-10-24 00:23:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,36 +4,37 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+// Base route handler
 app.get('/', (req, res) => {
   res.send("Welcome to the Home Renting API");
 });
 
+// Importing route handlers
+const authRoutes = require("./routes/auth.js");
+const listingRoutes = require("./routes/listing.js");
+const bookingRoutes = require("./routes/booking.js");
+const userRoutes = require("./routes/user.js");
 
-const authRoutes = require("./routes/auth.js")
-const listingRoutes = require("./routes/listing.js")
-const bookingRoutes = require("./routes/booking.js")
-const userRoutes = require("./routes/user.js")
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
 /* Routes */
-app.use("/api/auth",authRoutes)
-app.use("/api/properties",listingRoutes)
-app.use("/api/bookings", bookingRoutes)
-app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/properties", listingRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/users", userRoutes);
 
-/*Mongoose setup*/
+/* Mongoose setup */
 const PORT = process.env.PORT || 3001;
 mongoose.connect(process.env.MONGO_URL, {
-  dbName: "Dream_Nest",  // Specify database name explicitly here
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  dbName: "Dream_Nest",  // Specify database name explicitly
 })
 .then(() => {
   console.log("Successfully connected to MongoDB");
   app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 })
 .catch((err) => console.log(`MongoDB connection error: ${err.message}`));
+
 
